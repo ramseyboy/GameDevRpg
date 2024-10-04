5c362ee3c8b585658261196a0072794040b91df4
Walker Hannan <awahannan@gmail.com>
2024-10-04 17:35:29 -0500

    Initial RPG XNA/mono starter template running with apple silicon.
    Had to replace all font files, add contentless nuget pkg and a few other
    tweaks. Issue with reading and writing custom XML files as XNB to fix
    next

---
+++ RolePlayingGame/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new RolePlaying.RolePlayingGame();
+game.Run();
---
+++ RolePlayingGameProcessors/Characters/CharacterLevelDescriptionWriter.cs
@@ -0,0 +1,45 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// CharacterLevelDescriptionWriter.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+using RolePlayingGameData;
+#endregion
+
+namespace RolePlayingGameProcessors
+{
+    /// <summary>
+    /// This class will be instantiated by the XNA Framework Content Pipeline
+    /// to write the specified data type into binary .xnb format.
+    ///
+    /// This should be part of a Content Pipeline Extension Library project.
+    /// </summary>
+    [ContentTypeWriter]
+    public class CharacterLevelDescriptionWriter :
+        RolePlayingGameWriter<CharacterLevelDescription>
+    {
+        /// <inheritdoc />
+        public override string GetRuntimeReader(TargetPlatform targetPlatform) 
+            => typeof(CharacterLevelDescription.CharacterLevelDescriptionReader).AssemblyQualifiedName ?? string.Empty;
+        
+        protected override void Write(ContentWriter output, 
+            CharacterLevelDescription value)
+        {
+            output.Write(value.ExperiencePoints);
+            output.WriteObject(value.SpellContentNames);
+        }
+    }
+}
